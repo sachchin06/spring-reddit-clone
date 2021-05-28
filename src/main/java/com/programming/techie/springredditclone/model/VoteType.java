5dd32fded4e4975ebc09f96a99b2a6a7b8a90d4c
sachchin <sachchin06@gmail.com>
2021-05-28 13:41:26 +0530

    upload model and repository

---
+++ src/main/java/com/programming/techie/springredditclone/model/VoteType.java
@@ -0,0 +1,13 @@
+package com.programming.techie.springredditclone.model;
+
+
+public enum VoteType {
+    UPVOTE(1), DOWNVOTE(-1),
+    ;
+
+    private int direction;
+
+    VoteType(int direction) {
+    }
+
+}
